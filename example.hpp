aac019e3048f1509cb26bfbb5c17f5d14ce46091
jerrycheng567 <1292652198@qq.com>
2013-10-27 13:23:11 +0800

    add namespace, remove namespace pollution, bug fixed, refractored.

--- example.hpp
+++ example.hpp
@@ -4,146 +4,132 @@
 #include <memory>
 #include <utility>
 #include <cassert>
-using namespace std;
-
-struct proposition
+namespace gentzen_system
 {
-    enum symbol
-    {
-        logical_and, logical_or, logical_not, single_symbol
-    } s;
-    pair< const proposition *, const proposition * > p;
-    string str;
-    enum satisfiability
-    {
-        valid, satisfiable, unsatisfiable
-    };
-    satisfiability get_satisfiability( ) const
-    {
-        if ( s == single_symbol )
-        {
-            return satisfiable;
-        }
-        else
-        {
-            bool res = is_satisfiable( map< const proposition *, bool >( { make_pair( this, true ) } ) );
-            if ( res )
-            {
-                bool res = is_satisfiable( map< const proposition *, bool >( { make_pair( this, false ) } ) );
-                if ( res )
-                {
-                    return satisfiable;
-                }
-                else
-                {
-                    return valid;
-                }
-            }
-            else
-            {
-                return unsatisfiable;
-            }
-        }
-    }
-    static bool is_satisfiable( map< const proposition *, bool > && t )
-    {
-        auto tree( t );
-        map< string, bool > expanded_symbol;
-        while ( ! tree.empty( ) )
-        {
-            auto current_expand = tree.begin( );
-            if ( current_expand->first->s == single_symbol )
-            {
-                auto res = expanded_symbol.insert( make_pair( current_expand->first->str, current_expand->second ) );
-                if ( ( ! res.second ) && res.first->second != current_expand->second  ) { return false; }
-                tree.erase( current_expand );
-            }
-            else if ( current_expand->first->s == logical_and )
-            {
-                if ( current_expand->second )
-                {
-                    auto res = tree.insert( make_pair( current_expand->first->p.first, true ) );
-                    if ( ( ! res.second ) && ! res.first->second ) { return false; }
-                    {
-                        auto res = tree.insert( make_pair( current_expand->first->p.second, true ) );
-                        if ( ( ! res.second ) && ! res.first->second ) { return false; }
-                    }
-                    tree.erase( current_expand );
-                }
-                else
-                {
-                    const proposition * original_prop = current_expand->first;
-                    tree.erase( current_expand );
-                    struct insert_faliure{ };
-                    try {
-                        if ( is_satisfiable( [ & ]( )->decltype( tree ) {
-                                             auto new_tree( tree );
-                                             auto res = new_tree.insert( make_pair( original_prop->p.first, false ) );
-                                             if ( ( ! res.second ) && res.first->second ) { insert_faliure i_f; throw i_f; }
-                                             return new_tree; } ( ) ) )
-                        { return true; }
-                    } catch ( insert_faliure & ) { }
-                    auto res = tree.insert( make_pair( original_prop->p.second, false ) );
-                    if ( ( ! res.second ) && res.first->second ) { return false; }
-                }
-            }
-            else if ( current_expand->first->s == logical_or )
-            {
-                if ( current_expand->second )
-                {
-                    const proposition * original_prop = current_expand->first;
-                    tree.erase( current_expand );
-                    struct insert_faliure{ };
-                    try {
-                        if ( is_satisfiable( [ & ]( )->decltype( tree ) {
-                                             auto new_tree( tree );
-                                             auto res = new_tree.insert( make_pair( original_prop->p.first, true ) );
-                                             if ( ( ! res.second ) && ! res.first->second ) { insert_faliure i_f; throw i_f; }
-                                             return new_tree; } ( ) ) )
-                        { return true; }
-                    } catch ( insert_faliure & ) { }
-                    auto res = tree.insert( make_pair( original_prop->p.second, true ) );
-                    if ( ( ! res.second ) && ! res.first->second ) { return false; }
-                }
-                else
-                {
-                    auto res = tree.insert( make_pair( current_expand->first->p.first, false ) );
-                    if ( ( ! res.second ) && res.first->second ) { return false; }
-                    {
-                        auto res = tree.insert( make_pair( current_expand->first->p.second, false ) );
-                        if ( ( ! res.second ) && res.first->second ) { return false; }
-                    }
-                    tree.erase( current_expand );
-                }
-            }
-            else
-            {
-                auto res = tree.insert( make_pair( current_expand->first->p.first, ! current_expand->second ) );
-                if ( ( ! res.second ) && res.first->second == current_expand->second ) { return false; }
-                tree.erase( current_expand );
-            }
-        }
-        return true;
-    }
-    proposition( string && str ) : s( single_symbol ), str( str ) { }
-    proposition( symbol s, pair< const proposition *, const proposition * > && p ) : s( s ), p( p ) { }
-    static proposition make_or( const proposition & lhs, const proposition & rhs )
-    { return proposition( logical_or, make_pair( & lhs, & rhs ) ); }
-    static proposition make_and( const proposition & lhs, const proposition & rhs )
-    { return proposition( logical_and, make_pair( & lhs, & rhs ) ); }
-    static proposition make_not( const proposition & hs )
-    { return proposition( logical_not, make_pair( & hs, nullptr ) ); }
-};
+	using namespace std;
+	enum symbol
+	{ logical_and, logical_or, logical_not, single_symbol };
+	enum satisfiability
+	{ valid, satisfiable, unsatisfiable };
+	struct proposition
+	{
+		symbol s;
+		pair< const proposition *, const proposition * > p;
+		string str;
+		struct insert_faliure{ };
+		satisfiability get_satisfiability( ) const
+		{
+			if ( is_satisfiable( map< const proposition *, bool >( { make_pair( this, true ) } ) ) )
+			{
+				if ( is_satisfiable( map< const proposition *, bool >( { make_pair( this, false ) } ) ) )
+				{ return satisfiable; }
+				else
+				{ return valid; }
+			}
+			else
+			{ return unsatisfiable; }
+		}
+		struct example_tree
+		{
+			map< const proposition *, bool > sequent;
+			map< string, bool > expanded_symbol;
+			bool insert( const proposition * p, bool b )
+			{
+				auto res = sequent.insert( make_pair( p, b ) );
+				if ( ( ! res.second ) && res.first->second != b ) { return false; }
+				return true;
+			}
+			example_tree( const map< const proposition *, bool > & sequent, const map< string, bool > & expanded_symbol ) :
+				sequent( sequent ), expanded_symbol( expanded_symbol ) { }
+			example_tree( map< const proposition *, bool > && sequent ) : sequent( sequent ) { }
+			example_tree new_tree( const proposition * p, bool b )
+			{
+				example_tree nt( sequent, expanded_symbol );
+				if ( ! nt.insert( p, b ) ) { insert_faliure i_f; throw i_f; }
+				return nt;
+			}
+			bool is_satisfiable( )
+			{
+				while ( ! sequent.empty( ) )
+				{
+					auto current_expand = sequent.begin( );
+					const proposition * prop = current_expand->first;
+					const bool need_satisfy = current_expand->second;
+					sequent.erase( current_expand );
+					if ( prop->s == single_symbol )
+					{
+						auto res = expanded_symbol.insert( make_pair( prop->str, need_satisfy ) );
+						if ( ( ! res.second ) && res.first->second != need_satisfy ) { return false; }
+					}
+					else if ( prop->s == logical_and )
+					{
+						if ( current_expand->second )
+						{
+							if ( ( ! insert( prop->p.first, true ) ) ||
+									 ( ! insert( prop->p.second, true ) ) )
+							{ return false; }
+						}
+						else
+						{
+							try
+							{
+								if ( new_tree( prop->p.first, false ).is_satisfiable( ) )
+								{ return true; }
+							}
+							catch ( insert_faliure & ) { }
+							if ( ! insert( prop->p.second, false ) ) { return false; }
+						}
+					}
+					else if ( prop->s == logical_or )
+					{
+						if ( current_expand->second )
+						{
+							try
+							{
+								if ( new_tree( prop->p.first, true ).is_satisfiable( ) )
+								{ return true; }
+							}
+							catch ( insert_faliure & ) { }
+							if ( ! insert( prop->p.second, true ) ) { return false; }
+						}
+						else
+						{
+							if ( ( ! insert( prop->p.first, false ) ) ||
+									 ( ! insert( prop->p.second, false ) ) )
+							{ return false; }
+						}
+					}
+					else
+					{ if ( ! insert( prop->p.first, ! need_satisfy ) ) { return false; } }
+				}
+				return true;
+			}
+		};
+		static bool is_satisfiable( map< const proposition *, bool > && t )
+		{
+			example_tree et( move( t ) );
+			return et.is_satisfiable( );
+		}
+		proposition( string && str ) : s( single_symbol ), str( str ) { }
+		proposition( symbol s, pair< const proposition *, const proposition * > && p ) : s( s ), p( p ) { }
+		static proposition make_or( const proposition & lhs, const proposition & rhs )
+		{ return proposition( logical_or, make_pair( & lhs, & rhs ) ); }
+		static proposition make_and( const proposition & lhs, const proposition & rhs )
+		{ return proposition( logical_and, make_pair( & lhs, & rhs ) ); }
+		static proposition make_not( const proposition & hs )
+		{ return proposition( logical_not, make_pair( & hs, nullptr ) ); }
+	};
 
-int example( )
-{
-    proposition A( "A" );
-    proposition not_a( proposition::make_not( A ) );
-    auto res1 = A.get_satisfiability( );
-    auto res2 = proposition::make_or( A, not_a ).get_satisfiability( );
-    auto res3 = proposition::make_and( A, not_a ).get_satisfiability( );
-    if ( res1 == proposition::satisfiable && res2 == proposition::valid && res3 == proposition::unsatisfiable )
-    { cout << "Hello World!" << endl; }
-    return 0;
+	int example( )
+	{
+		proposition A( "A" );
+		proposition not_a( proposition::make_not( A ) );
+		auto res1 = A.get_satisfiability( );
+		auto res2 = proposition::make_or( A, not_a ).get_satisfiability( );
+		auto res3 = proposition::make_and( A, not_a ).get_satisfiability( );
+		if ( res1 == satisfiable && res2 == valid && res3 == unsatisfiable )
+		{ cout << "Hello World!" << endl; }
+		return 0;
+	}
+
 }
-
