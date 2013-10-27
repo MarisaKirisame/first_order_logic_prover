9cf0c029860f5a8459bc3357308bba71f2075227
jerrycheng567 <1292652198@qq.com>
2013-10-27 16:17:19 +0800

    add imply and equal, refractored code

--- example.hpp
+++ example.hpp
@@ -11,38 +11,31 @@ namespace gentzen_system
 	{ logical_and, logical_or, logical_not, single_symbol };
 	enum satisfiability
 	{ valid, satisfiable, unsatisfiable };
-	struct proposition
+	struct in_proposition
 	{
 		symbol s;
-		pair< const proposition *, const proposition * > p;
+		pair< const shared_ptr< in_proposition >, const shared_ptr< in_proposition > > p;
 		string str;
+		in_proposition( string && str ) : s( single_symbol ), str( str ) { }
+		in_proposition( symbol s, pair< const shared_ptr< in_proposition >, const shared_ptr< in_proposition > > && p ) : s( s ), p( p ) { }
+	};
+	struct proposition : shared_ptr< in_proposition >
+	{
 		struct insert_faliure{ };
-		satisfiability get_satisfiability( ) const
-		{
-			if ( is_satisfiable( map< const proposition *, bool >( { make_pair( this, true ) } ) ) )
-			{
-				if ( is_satisfiable( map< const proposition *, bool >( { make_pair( this, false ) } ) ) )
-				{ return satisfiable; }
-				else
-				{ return valid; }
-			}
-			else
-			{ return unsatisfiable; }
-		}
 		struct example_tree
 		{
-			map< const proposition *, bool > sequent;
+			map< const shared_ptr< in_proposition >, bool > sequent;
 			map< string, bool > expanded_symbol;
-			bool insert( const proposition * p, bool b )
+			bool insert( const shared_ptr< in_proposition > p, bool b )
 			{
 				auto res = sequent.insert( make_pair( p, b ) );
 				if ( ( ! res.second ) && res.first->second != b ) { return false; }
 				return true;
 			}
-			example_tree( const map< const proposition *, bool > & sequent, const map< string, bool > & expanded_symbol ) :
+			example_tree( const map< const shared_ptr< in_proposition >, bool > & sequent, const map< string, bool > & expanded_symbol ) :
 				sequent( sequent ), expanded_symbol( expanded_symbol ) { }
-			example_tree( map< const proposition *, bool > && sequent ) : sequent( sequent ) { }
-			example_tree new_tree( const proposition * p, bool b )
+			example_tree( map< const shared_ptr< in_proposition >, bool > && sequent ) : sequent( sequent ) { }
+			example_tree new_tree( const shared_ptr< in_proposition > p, bool b )
 			{
 				example_tree nt( sequent, expanded_symbol );
 				if ( ! nt.insert( p, b ) ) { insert_faliure i_f; throw i_f; }
@@ -53,7 +46,7 @@ namespace gentzen_system
 				while ( ! sequent.empty( ) )
 				{
 					auto current_expand = sequent.begin( );
-					const proposition * prop = current_expand->first;
+					const shared_ptr< in_proposition > prop = current_expand->first;
 					const bool need_satisfy = current_expand->second;
 					sequent.erase( current_expand );
 					if ( prop->s == single_symbol )
@@ -105,29 +98,48 @@ namespace gentzen_system
 				return true;
 			}
 		};
-		static bool is_satisfiable( map< const proposition *, bool > && t )
+		proposition( in_proposition * p ) : shared_ptr< in_proposition >( p ) { }
+		satisfiability get_satisfiability( )
+		{
+			if ( is_satisfiable( map< const shared_ptr< in_proposition >, bool >( { make_pair( * this, true ) } ) ) )
+			{
+				if ( is_satisfiable( map< const shared_ptr< in_proposition >, bool >( { make_pair( * this, false ) } ) ) )
+				{ return satisfiable; }
+				else
+				{ return valid; }
+			}
+			else
+			{ return unsatisfiable; }
+		}
+		static bool is_satisfiable( map< const shared_ptr< in_proposition >, bool > && t )
 		{
 			example_tree et( move( t ) );
 			return et.is_satisfiable( );
 		}
-		proposition( string && str ) : s( single_symbol ), str( str ) { }
-		proposition( symbol s, pair< const proposition *, const proposition * > && p ) : s( s ), p( p ) { }
-		static proposition make_or( const proposition & lhs, const proposition & rhs )
-		{ return proposition( logical_or, make_pair( & lhs, & rhs ) ); }
-		static proposition make_and( const proposition & lhs, const proposition & rhs )
-		{ return proposition( logical_and, make_pair( & lhs, & rhs ) ); }
-		static proposition make_not( const proposition & hs )
-		{ return proposition( logical_not, make_pair( & hs, nullptr ) ); }
 	};
+
+	static proposition make_or( const proposition & lhs, const proposition & rhs )
+	{ return proposition( new in_proposition( logical_or, make_pair( lhs, rhs ) ) ); }
+	static proposition make_and( const shared_ptr< in_proposition > lhs, const shared_ptr< in_proposition > rhs )
+	{ return proposition( new in_proposition( logical_and, make_pair( lhs, rhs ) ) ); }
+	static proposition make_not( const shared_ptr< in_proposition > hs )
+	{ return proposition( new in_proposition( logical_not, make_pair( hs, nullptr ) ) ); }
+	static proposition make_equal( const proposition & lhs, const proposition & rhs )
+	{ return make_or( make_and( lhs, rhs ), make_and( make_not( lhs ), make_not( rhs ) ) ); }
+	static proposition make_imply( const proposition & lhs, const proposition & rhs )
+	{ return make_or( make_not( lhs ), rhs ); }
 
 	int example( )
 	{
-		proposition A( "A" );
-		proposition not_a( proposition::make_not( A ) );
+		proposition A( new in_proposition( "A" ) );
+		proposition not_a( make_not( A ) );
+		proposition valid_prop( make_or( A, not_a ) );
+		proposition unsatisfiable_prop( make_and( A, not_a ) );
 		auto res1 = A.get_satisfiability( );
-		auto res2 = proposition::make_or( A, not_a ).get_satisfiability( );
-		auto res3 = proposition::make_and( A, not_a ).get_satisfiability( );
-		if ( res1 == satisfiable && res2 == valid && res3 == unsatisfiable )
+		auto res2 = valid_prop.get_satisfiability( );
+		auto res3 = unsatisfiable_prop.get_satisfiability( );
+		if ( res1 == satisfiable && res2 == valid && res3 == unsatisfiable && make_equal( A, A ).get_satisfiability( ) == valid &&
+				 make_and( make_not( make_imply( A, not_a ) ), not_a ).get_satisfiability( ) == unsatisfiable )
 		{ cout << "Hello World!" << endl; }
 		return 0;
 	}
