a336283a3f8276483ad3c21d45f55c4f923afdc0
lolisa <1292652198@qq.com>
2014-10-13 21:27:30 +0800

    reindent

--- praser.hpp
+++ praser.hpp
@@ -19,108 +19,120 @@
 #include "variable.hpp"
 namespace first_order_logic
 {
-	namespace
-	{
-		namespace spirit = boost::spirit;
-		namespace qi = spirit::qi;
-		namespace encoding  = boost::spirit::unicode;
-		template< typename IT >
-		struct FOL_grammar : qi::grammar< IT, free_sentence( ), encoding::space_type >
-		{
-			FOL_grammar( ) : FOL_grammar::base_type( expression )
-			{
-				using qi::lit;
-				using qi::lexeme;
-				using encoding::char_;
-				using encoding::string;
-				using encoding::alnum;
-				using encoding::alpha;
-				using encoding::blank;
-				using encoding::digit;
-				using encoding::lower;
-				using namespace qi::labels;
-				namespace fusion = boost::fusion;
-				namespace phoenix = boost::phoenix;
-				namespace qi = boost::spirit::qi;
-				namespace ascii = boost::spirit::ascii;
-				using phoenix::at_c;
-				using phoenix::push_back;
-				using phoenix::bind;
-				using phoenix::construct;
-				text %= lexeme[alpha>>*(alnum)];
-				parse_propositional_letter = text[ _val = bind( make_propositional_letter, _1 ) ];
-				bool_exp %=
-						( lit( '(' ) >> expression >> lit( ')' ) ) |
-						with_equality |
-						predicate |
-						parse_propositional_letter;
-				with_equality =
-						( term_exp >> lit( '=' ) >> term_exp )
-						[ bind( []( auto & val, auto & l, auto & r ){ val = make_equal( l, r ); }, _val, _1, _2 ) ];
-				predicate =
-						( text >> lit( '(' ) >> ( term_exp % ',' ) >> lit( ')' ) )[ _val = bind( make_predicate, _1, _2 ) ];
-				with_not =
-						bool_exp[ _val = _1 ] |
-						( lit( '!' ) >> with_not )
-						[ bind( []( auto & val, auto & param ){ val = make_not( param ); }, _val, _1 ) ];
-				with_binary =
-						with_not[ _val = _1 ] >> *
-						(
-							( lit( "/\\" ) >> with_not )
-							[ bind( []( auto & val, auto & param ){ val = make_and( val, param ); }, _val, _1 ) ] |
-							( ( lit( "\\/" ) >> with_not )
-							[ bind( []( auto & val, auto & param ){ val = make_or( val, param ); }, _val, _1 ) ] )
-						);
-				with_quantifier =
-						( lit( u8"∃" ) >> parse_variable >> expression )
-						[ bind( []( auto & val, auto & param1, auto & param2 )
-						{ val = make_some( param1, param2 ); }, _val, _1, _2 ) ] |
-						( lit( u8"∀" ) >> parse_variable >> expression )
-						[ bind( []( auto & val, auto & param1, auto & param2 )
-						{ val = make_all( param1, param2 ); }, _val, _1, _2 ) ] |
-						with_binary[ _val = _1 ];
-				with_implication =
-						with_quantifier[ _val = _1 ] >> *
-						(
-							( lit( "->" ) >> with_quantifier )
-							[ bind( []( auto & val, auto & param ){ val = make_imply( val, param ); }, _val, _1 ) ] |
-							( lit( "<-" ) >> with_quantifier )
-							[ bind( []( auto & val, auto & param ){ val = make_imply( param, val ); }, _val, _1 ) ] |
-							( lit( "<->" ) >> with_quantifier )
-							[ bind( []( auto & val, auto & param ){ val = make_iff( val, param ); }, _val, _1 ) ]
-						);
-				expression %= with_implication;
-				term_exp = function[ _val = _1 ] | text[ _val = bind( make_variable, _1 ) ];
-				function =
-						( text >> lit( '(' ) >> ( term_exp % ',' ) >> lit( ')' ) )[ _val = bind( make_function, _1, _2 ) ];
-				parse_variable = text[ _val = construct< variable >( _1 ) ];
-			}
-			qi::rule< IT, variable( ), encoding::space_type > parse_variable;
-			qi::rule< IT, term( ), encoding::space_type >
-				function,
-				term_exp;
-			qi::rule< IT, free_sentence( ), encoding::space_type >
-				with_equality,
-				predicate,
-				expression,
-				bool_exp,
-				with_not,
-				with_binary,
-				with_quantifier,
-				parse_propositional_letter,
-				with_implication;
-			qi::rule< IT, std::string( ), encoding::space_type> text;
-		};
-	}
-	boost::optional< free_sentence > prase( const std::string & s )
-	{
-		auto i = s.begin( );
-		auto e = s.end( );
-		free_sentence ret;
-		FOL_grammar< decltype( i ) > fol;
-		bool succeed = boost::spirit::qi::phrase_parse( i, e, fol, boost::spirit::unicode::space, ret );
-		if ( ! ( succeed && i == e ) ) { return boost::optional< free_sentence >( ); }
-		return ret;
-	}
-}
+    namespace
+    {
+        namespace spirit = boost::spirit;
+        namespace qi = spirit::qi;
+        namespace encoding  = boost::spirit::unicode;
+        template< typename IT >
+        struct FOL_grammar : qi::grammar< IT, free_sentence( ), encoding::space_type >
+        {
+            FOL_grammar( ) : FOL_grammar::base_type( expression )
+            {
+                using qi::lit;
+                using qi::lexeme;
+                using encoding::char_;
+                using encoding::string;
+                using encoding::alnum;
+                using encoding::alpha;
+                using encoding::blank;
+                using encoding::digit;
+                using encoding::lower;
+                using namespace qi::labels;
+                namespace fusion = boost::fusion;
+                namespace phoenix = boost::phoenix;
+                namespace qi = boost::spirit::qi;
+                namespace ascii = boost::spirit::ascii;
+                using phoenix::at_c;
+                using phoenix::push_back;
+                using phoenix::bind;
+                using phoenix::construct;
+                text %= lexeme[alpha>>*(alnum)];
+                parse_propositional_letter = text[ _val = bind( make_propositional_letter, _1 ) ];
+                bool_exp %=
+                    ( lit( '(' ) >> expression >> lit( ')' ) ) |
+                    with_equality |
+                    predicate |
+                    parse_propositional_letter;
+                with_equality =
+                    ( term_exp >> lit( '=' ) >> term_exp )
+                    [ bind( []( auto & val, auto & l, auto & r ){ val = make_equal( l, r ); }, _val, _1, _2 ) ];
+                predicate =
+                    ( text >> lit( '(' ) >> ( term_exp % ',' ) >> lit( ')' ) )[ _val = bind( make_predicate, _1, _2 ) ];
+                with_not =
+                    bool_exp[ _val = _1 ] |
+                        ( lit( '!' ) >> with_not )
+                        [ bind( []( auto & val, auto & param ){ val = make_not( param ); }, _val, _1 ) ];
+                with_binary =
+                    with_not[ _val = _1 ] >> *
+                    (
+                        ( lit( "/\\" ) >> with_not )
+                            [ bind( []( auto & val, auto & param ){ val = make_and( val, param ); }, _val, _1 ) ] |
+                        ( ( lit( "\\/" ) >> with_not )
+                            [ bind( []( auto & val, auto & param ){ val = make_or( val, param ); }, _val, _1 ) ] )
+                    );
+                with_quantifier =
+                    ( lit( u8"∃" ) >> parse_variable >> expression )
+                        [
+                            bind(
+                                []( auto & val, auto & param1, auto & param2 )
+                                    { val = make_some( param1, param2 ); },
+                                _val,
+                                _1,
+                                _2 )
+                        ] |
+                    ( lit( u8"∀" ) >> parse_variable >> expression )
+                        [
+                            bind(
+                                []( auto & val, auto & param1, auto & param2 )
+                                    { val = make_all( param1, param2 ); },
+                                _val,
+                                _1,
+                                _2 )
+                        ] |
+                with_binary[ _val = _1 ];
+                with_implication =
+                with_quantifier[ _val = _1 ] >> *
+                (
+                    ( lit( "->" ) >> with_quantifier )
+                        [ bind( []( auto & val, auto & param ){ val = make_imply( val, param ); }, _val, _1 ) ] |
+                    ( lit( "<-" ) >> with_quantifier )
+                        [ bind( []( auto & val, auto & param ){ val = make_imply( param, val ); }, _val, _1 ) ] |
+                    ( lit( "<->" ) >> with_quantifier )
+                        [ bind( []( auto & val, auto & param ){ val = make_iff( val, param ); }, _val, _1 ) ]
+                );
+                expression %= with_implication;
+                term_exp = function[ _val = _1 ] | text[ _val = bind( make_variable, _1 ) ];
+                function =
+                ( text >> lit( '(' ) >> ( term_exp % ',' ) >> lit( ')' ) )[ _val = bind( make_function, _1, _2 ) ];
+                parse_variable = text[ _val = construct< variable >( _1 ) ];
+            }
+            qi::rule< IT, variable( ), encoding::space_type > parse_variable;
+            qi::rule< IT, term( ), encoding::space_type >
+                function,
+                term_exp;
+                qi::rule< IT, free_sentence( ), encoding::space_type >
+                with_equality,
+                predicate,
+                expression,
+                bool_exp,
+                with_not,
+                with_binary,
+                with_quantifier,
+                parse_propositional_letter,
+                with_implication;
+                qi::rule< IT, std::string( ), encoding::space_type> text;
+        };
+    }
+        boost::optional< free_sentence > prase( const std::string & s )
+        {
+            auto i = s.begin( );
+            auto e = s.end( );
+            free_sentence ret;
+            FOL_grammar< decltype( i ) > fol;
+            bool succeed = boost::spirit::qi::phrase_parse( i, e, fol, boost::spirit::unicode::space, ret );
+            if ( ! ( succeed && i == e ) ) { return boost::optional< free_sentence >( ); }
+            return ret;
+        }
+    }
 #endif // FIRST_ORDER_LOGIC_PRASER_HPP
