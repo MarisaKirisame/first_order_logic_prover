cfaa3c061b097d97e2b990989fa7b02d5b820fdf
lolisa <lolisa@marisa.moe>
2015-03-11 08:53:28 -0400

    Change tab to space.

--- constant.hpp
+++ constant.hpp
@@ -3,15 +3,15 @@
 #include "constant.hpp"
 namespace first_order_logic
 {
-	struct constant
-	{
-		std::string name;
-		constant( const std::string & str ) : name( str ) { }
-		template< typename T >
-		explicit constant( const T & t ) : name( t ) { }
-		explicit constant( ) { }
-		explicit operator std::string( ) const { return name; }
-		bool operator < ( const constant & comp ) const { return name < comp.name; }
-	};
+    struct constant
+    {
+        std::string name;
+        constant( const std::string & str ) : name( str ) { }
+        template< typename T >
+        explicit constant( const T & t ) : name( t ) { }
+        explicit constant( ) { }
+        explicit operator std::string( ) const { return name; }
+        bool operator < ( const constant & comp ) const { return name < comp.name; }
+    };
 }
 #endif // CONSTANTS_HPP
